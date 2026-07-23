fe18caf791dc952f8ac76fce0924619395b27215
Andrew Torrez <torrez_andrew@outlook.com>
2026-07-23 13:17:37 -0700

    Create intial job description word counter

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Job Search Copilot")
+print("------------------")
+
+job_description = input("Paste a short job description: ")
+
+word_count = len(job_description.split())
+
+print(f"This job description contaions {word_count} words.")
